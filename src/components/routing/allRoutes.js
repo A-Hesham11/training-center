43c77c3d8f470c50218de45d8b0a2d77f071ba10
A-Hesham11 <a.hesham2215@gmail.com>
2024-01-25 03:17:51 +0200

    nav

--- src/components/routing/allRoutes.js
+++ src/components/routing/allRoutes.js
@@ -1,90 +1,118 @@
 import React, { Fragment } from "react";
 import { Route, Routes } from "react-router-dom";
-import NavBar from "../navBar/NavBar";
 import Footer from "../footer/Footer";
 import HomePage from "../../pages/HomePage";
 import Courses from "../../pages/Courses";
 import Trainers from "../../pages/Trainers";
 import AboutUs from "../../pages/AboutUs";
 import CallUs from "../../pages/CallUs";
-<<<<<<< HEAD
 import TermsAndConditions from "../../pages/TermsAndConditions";
 import PublicPolicies from "../../pages/PublicPolicies";
 import TechnicalSupport from "../../pages/TechnicalSupport";
 import ComplaintsSuggestions from "../../pages/ComplaintsSuggestions";
-=======
 import SignInForm from "../../pages/SignInForm";
 import RegisterForm from "../../pages/RegisterForm";
->>>>>>> fa58e81092a53363b720f2ccaa037a4084eb4a0b
 
 const AllRoutesProvider = () => {
   return (
     <Routes>
-      <Route path="/" element={
-        <Fragment>
+      <Route
+        path="/"
+        element={
+          <Fragment>
             <HomePage />
             <Footer />
-        </Fragment>
-      } />
-      <Route path="/الدورات" element={
-        <Fragment>
+          </Fragment>
+        }
+      />
+      <Route
+        path="/الدورات"
+        element={
+          <Fragment>
             <Courses />
             <Footer />
-        </Fragment>
-      } />
-      <Route path="/المدربين" element={
-        <Fragment>
+          </Fragment>
+        }
+      />
+      <Route
+        path="/المدربين"
+        element={
+          <Fragment>
             <Trainers />
             <Footer />
-        </Fragment>
-      } />
-      <Route path="/من_نحن" element={
-        <Fragment>
+          </Fragment>
+        }
+      />
+      <Route
+        path="/من_نحن"
+        element={
+          <Fragment>
             <AboutUs />
             <Footer />
-        </Fragment>
-      } />
-      <Route path="/إتصل_بنا" element={
-        <Fragment>
+          </Fragment>
+        }
+      />
+      <Route
+        path="/إتصل_بنا"
+        element={
+          <Fragment>
             <CallUs />
             <Footer />
-        </Fragment>
-      } />
-<<<<<<< HEAD
-      <Route path="/الشروط" element={
-        <Fragment>
+          </Fragment>
+        }
+      />
+      <Route
+        path="/الشروط"
+        element={
+          <Fragment>
             <TermsAndConditions />
             <Footer />
-        </Fragment>
-      } />
-      <Route path="/السياسات" element={
-        <Fragment>
+          </Fragment>
+        }
+      />
+      <Route
+        path="/السياسات"
+        element={
+          <Fragment>
             <PublicPolicies />
             <Footer />
-        </Fragment>
-      } />
-      <Route path="/الدعم_الفني" element={
-        <Fragment>
+          </Fragment>
+        }
+      />
+      <Route
+        path="/الدعم_الفني"
+        element={
+          <Fragment>
             <TechnicalSupport />
             <Footer />
-        </Fragment>
-      } />
-      <Route path="/الشكاوي" element={
-        <Fragment>
+          </Fragment>
+        }
+      />
+      <Route
+        path="/الشكاوي"
+        element={
+          <Fragment>
             <ComplaintsSuggestions />
             <Footer />
-=======
-      <Route path="/التسجيل" element={
-        <Fragment>
+          </Fragment>
+        }
+      />
+      <Route
+        path="/التسجيل"
+        element={
+          <Fragment>
             <SignInForm />
-        </Fragment>
-      } />
-      <Route path="/إنشاء_حساب" element={
-        <Fragment>
+          </Fragment>
+        }
+      />
+      <Route
+        path="/إنشاء_حساب"
+        element={
+          <Fragment>
             <RegisterForm />
->>>>>>> fa58e81092a53363b720f2ccaa037a4084eb4a0b
-        </Fragment>
-      } />
+          </Fragment>
+        }
+      />
     </Routes>
   );
 };
